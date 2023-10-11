e43081368666a0545577584917e6a9d0d2d4d4dc
Mauregina <mauraregina@gmail.com>
2023-10-11 15:57:18 -0500

    add getCssText in order to give all the CSS needed to SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
 
+import { styled } from '../styles'
+
+const Button = styled('button', {
+  backgroundColor: '$rocketSeat',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px'
+})
+
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <Button>Hello world</Button>
   )
 }
---
+++ src/styles/index.ts
@@ -0,0 +1,17 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+    config,
+    styled,
+    globalCss,
+    keyframes,
+    getCssText,
+    theme,
+    createTheme
+} = createStitches({
+    theme: {
+        colors: {
+            rocketSeat: '#8D8686'
+        }
+    }
+})
