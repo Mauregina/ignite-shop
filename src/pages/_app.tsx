1c3152534d808b5ebb70be80699411742bc719d0
Mauregina <mauraregina@gmail.com>
2023-10-11 17:12:03 -0500

    add style to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,17 @@ import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 
 import logo from '../assets/logo.svg'
+import { Container, Header } from '@/styles/pages/app'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <header>
+    <Container>
+      <Header>
         <img src={logo.src} alt="" />
-      </header>
+      </Header>
       <Component {...pageProps} />
-    </div>
+    </Container>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,3 @@
-import { styled } from '../styles'
-
-const Button = styled('button', {
-  backgroundColor: '$gray3',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-})
-
 export default function Home() {
-  return <Button>Hello world</Button>
+  return <div>Hello world</div>
 }
