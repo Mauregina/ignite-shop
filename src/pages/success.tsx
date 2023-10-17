d77751c69f513d143e0c3efa03bf5dd810438eaa
Mauregina <mauraregina@gmail.com>
2023-10-17 15:41:51 -0500

    add style to success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,18 @@
+import Link from 'next/link'
+
+import { ImageContainer, SuccessContainer } from '@/styles/pages/success'
+
 export default function Success() {
-  return <h1>Success</h1>
+  return (
+    <SuccessContainer>
+      <h1>Compra Efetuada!</h1>
+      <ImageContainer></ImageContainer>
+      <p>
+        Uhuul <strong>Diego Fernandes</strong> , sua{' '}
+        <strong>compra de 3 camisetas</strong> já está a caminho da sua casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  )
 }
